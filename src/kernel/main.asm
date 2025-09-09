d2980c8eaab8fbd68f50d37a4137fe71c6b7b740
Harry Fellowes <harryfellowes1@gmail.com>
2025-09-09 15:22:59 +0100

    refactor: indentation
    Make indentation to be 1 tab that is 8 in width.
    Applied to src/bootloader/boot.asm & src/kernel/main.asm
    
    Signed-off-by: Harry Fellowes <harryfellowes1@gmail.com>

--- src/kernel/main.asm
+++ src/kernel/main.asm
@@ -6,40 +6,41 @@ bits 16
 
 
 start:
-    ; print hello world message
-    mov si, KERNMSG
-    call puts
+	; print hello world message
+	mov si, KERNMSG
+	call puts
 
 .halt:
-    cli
-    hlt
+	cli
+	hlt
 
 ;
 ; Prints a string to the screen
 ; Params:
 ;   - ds:si points to string
 ;
+
 puts:
-    ; save registers we will modify
-    push si
-    push ax
-    push bx
+	; save registers we will modify
+	push si
+	push ax
+	push bx
 
 .loop:
-    lodsb               ; loads next character in al
-    or al, al           ; verify if next character is null?
-    jz .done
+	lodsb			; loads next character in al
+	or al, al		; verify if next character is null?
+	jz .done
 
-    mov ah, 0x0E        ; call bios interrupt
-    mov bh, 0           ; set page number to 0
-    int 0x10
+	mov ah, 0x0E		; call bios interrupt
+	mov bh, 0		; set page number to 0
+	int 0x10
 
-    jmp .loop
+	jmp .loop
 
 .done:
-    pop bx
-    pop ax
-    pop si
-    ret
+	pop bx
+	pop ax
+	pop si
+	ret
 
-KERNMSG: db 'Kernel: OK', ENDL, 0
+KERNMSG:	db 'Kernel: OK', ENDL, 0
